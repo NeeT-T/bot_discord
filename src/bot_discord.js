0c2da370feb9504e00cf2fd0582928d9207792ff
Renato S. Oliveira <renato.santojpg@gmail.com>
2021-07-17 09:03:11 -0300

    That's worst, I need better it

--- src/bot_discord.js
+++ src/bot_discord.js
@@ -1,11 +1,15 @@
-const Discord = require('discord.js');
-require('dotenv').config();
-
-const clientBot = new Discord.Client();
-const ytdl = require('ytdl-core');
-const musicOptions = {seek: 0, volume: 1};
+import {search as searchYoutubeVideoByName} from './services/YoutubeAccess.js';
+import {Client} from 'discord.js';
+import {} from 'dotenv/config';
+import ytdl from 'ytdl-core';
 
 const CHANNEL_GERAL = process.env.DISCORD_VOICE_CHANNEL_ID;
+const CHANNEL_ENABLE_TO_COMMAND = process.env.DISCORD_ENABLE_MASSAGE_CHANNEL_ID;
+const SONG_IDS = [];
+let dispatcher = null;
+
+const clientBot = new Client();
+const musicOptions = {type: 'webm/opus', seek: 0, volume: 1, plp: 0, fec: true};
 
 clientBot.login(process.env.TOKEN_BOT_DISCORD);
 
@@ -13,27 +17,61 @@ clientBot.on('ready', () => {
   console.log(`Logged in as ${clientBot.user.tag}!`);
 });
 
-clientBot.on('message', (msg) => {
-  if (msg.author.clientBot) return;
-  // console.log(msg.guild.channels.cache);
-  if (msg.content.toLowerCase().startsWith('!play')) {
-    const voiceChannel = msg.guild.channels.cache
-        .find((channels) => channels.id === CHANNEL_GERAL);
-    if (voiceChannel === null) {
-      console.log('Voice channel not find');
-      return;
-    } else {
-      voiceChannel.join()
-          .then((connection) => {
-            const music = ytdl('https://www.youtube.com/watch?v=wqS55NBihCc&ab_channel=MartinPeralta', {filter: 'audioonly'});
-            const DJ = connection.play(music, musicOptions);
-            DJ.on('end', (end) => {
-              console.log('Acabou de tocar');
-            // voiceChannel.leave();
-            });
-          })
-          .catch(console.error);
-    }
+clientBot.on('message', async (msg) => {
+  const voiceChannel = msg?.guild?.channels?.cache
+      .find((channels) => channels?.id === CHANNEL_GERAL);
+  if (msg?.author?.clientBot ||
+    msg?.channel?.id !== CHANNEL_ENABLE_TO_COMMAND) {
+    return;
+  } else if (msg?.content?.trim()?.toLowerCase().startsWith('!play')) {
+    try {
+      await makeSearch(msg?.content?.split('!play')[1]?.trim() ?? null);
+      if (!SONG_IDS?.length) return;
+      if (voiceChannel) {
+        try {
+          if (SONG_IDS?.length === 1) {
+            await startMusic(voiceChannel);
+          } else return;
+          dispatcher.on('finish', async () => {
+            SONG_IDS.shift();
+            (SONG_IDS?.length) ? await startMusic(voiceChannel) :
+              await voiceChannel.leave();
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        console.log(`Voice channel not found`);
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+    };
+  } else if (msg?.content?.trim()?.toLowerCase().startsWith('!next')) {
+    if (SONG_IDS?.length > 1) {
+      SONG_IDS.shift();
+      await startMusic(voiceChannel);
+    } else return;
   }
 });
 
+const makeSearch = async (nameToSearch = null) => {
+  if (!nameToSearch) return;
+  try {
+    const result = await searchYoutubeVideoByName(nameToSearch);
+    const videoId = result?.data?.items[0]?.id?.videoId ?? null;
+    (videoId) ? SONG_IDS.push(videoId) : null;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const startMusic = async (voiceChannel) => {
+  try {
+    const connection = await voiceChannel.join();
+    const music = ytdl(`https://www.youtube.com/watch?v=${SONG_IDS[0]}`, {filter: 'audioonly'});
+    dispatcher = connection.play(music, musicOptions);
+  } catch (error) {
+    console.error(error);
+  }
+};
