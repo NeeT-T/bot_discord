386b44a79fe7d4872262a90e5b4d77e59d9a0b68
Renato S. Oliveira <renato.santojpg@gmail.com>
2021-07-19 19:24:09 -0300

    Updating bot, add music queue, next and list command

--- src/bot_discord.js
+++ src/bot_discord.js
@@ -5,73 +5,106 @@ import ytdl from 'ytdl-core';
 
 const CHANNEL_GERAL = process.env.DISCORD_VOICE_CHANNEL_ID;
 const CHANNEL_ENABLE_TO_COMMAND = process.env.DISCORD_ENABLE_MASSAGE_CHANNEL_ID;
-const SONG_IDS = [];
-let dispatcher = null;
+let isPlaying = false;
+const SONG_LIST_NAMES = [];
 
-const clientBot = new Client();
-const musicOptions = {type: 'webm/opus', seek: 0, volume: 1, plp: 0, fec: true};
+const bot = new Client();
+const musicOptions = {seek: 0, volume: 1, plp: 0, fec: true};
 
-clientBot.login(process.env.TOKEN_BOT_DISCORD);
+bot.login(process.env.TOKEN_BOT_DISCORD);
 
-clientBot.on('ready', () => {
-  console.log(`Logged in as ${clientBot.user.tag}!`);
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
 });
 
-clientBot.on('message', async (msg) => {
-  const voiceChannel = msg?.guild?.channels?.cache
-      .find((channels) => channels?.id === CHANNEL_GERAL);
-  if (msg?.author?.clientBot ||
+bot.on('message', async (msg) => {
+  if (msg?.author?.bot ||
     msg?.channel?.id !== CHANNEL_ENABLE_TO_COMMAND) {
     return;
-  } else if (msg?.content?.trim()?.toLowerCase().startsWith('!play')) {
-    try {
-      await makeSearch(msg?.content?.split('!play')[1]?.trim() ?? null);
-      if (!SONG_IDS?.length) return;
-      if (voiceChannel) {
-        try {
-          if (SONG_IDS?.length === 1) {
-            await startMusic(voiceChannel);
-          } else return;
-          dispatcher.on('finish', async () => {
-            SONG_IDS.shift();
-            (SONG_IDS?.length) ? await startMusic(voiceChannel) :
-              await voiceChannel.leave();
-          });
-        } catch (error) {
-          console.error(error);
+  } else {
+    const voiceChannel = msg?.guild?.channels?.cache
+        .find((channels) => channels?.id === CHANNEL_GERAL);
+    const value = msg?.content?.trim()?.toLowerCase()?.split(' ');
+    switch (value[0]) {
+      case '!play':
+        value.shift();
+        if (voiceChannel) {
+          const musicTitle = value?.join(' ')?.trim()?.toLowerCase() ?? null;
+          if (musicTitle) {
+            SONG_LIST_NAMES.push(musicTitle);
+            if (bot?.voice?.connections?.find((connection) =>
+              connection?.channel?.id === CHANNEL_GERAL)) {
+              if (!isPlaying) {
+                await searchMusicByNameOnYoutube(voiceChannel,
+                    SONG_LIST_NAMES[0]);
+              }
+            } else {
+              await searchMusicByNameOnYoutube(voiceChannel,
+                  SONG_LIST_NAMES[0]);
+            }
+          } else {
+            return;
+          }
+        } else {
+          console.log(`Voice channel not found`);
+          return;
         }
-      } else {
-        console.log(`Voice channel not found`);
-        return;
-      }
-    } catch (error) {
-      console.error(error);
-    };
-  } else if (msg?.content?.trim()?.toLowerCase().startsWith('!next')) {
-    if (SONG_IDS?.length > 1) {
-      SONG_IDS.shift();
-      await startMusic(voiceChannel);
-    } else return;
+        break;
+      case '!next':
+        await nextMusic(voiceChannel);
+        break;
+      case '!list':
+        SONG_LIST_NAMES?.length ?
+          SONG_LIST_NAMES?.map((music, index) => {
+          index === 0 ?
+              msg?.channel?.send(`${music} - Play now`) :
+              msg?.channel?.send(`${music}`);
+          }) :
+          msg?.channel?.send('Não a nenhuma musica na lista de reprodução');
+        break;
+      case '':
+        break;
+    }
   }
 });
 
-const makeSearch = async (nameToSearch = null) => {
+const searchMusicByNameOnYoutube = async (voiceChannel,
+    nameToSearch = null) => {
   if (!nameToSearch) return;
   try {
+    isPlaying = true;
     const result = await searchYoutubeVideoByName(nameToSearch);
     const videoId = result?.data?.items[0]?.id?.videoId ?? null;
-    (videoId) ? SONG_IDS.push(videoId) : null;
+    await startMusic(voiceChannel, videoId);
   } catch (error) {
     console.error(error);
   }
 };
 
-const startMusic = async (voiceChannel) => {
+const startMusic = async (voiceChannel, videoId = null) => {
   try {
-    const connection = await voiceChannel.join();
-    const music = ytdl(`https://www.youtube.com/watch?v=${SONG_IDS[0]}`, {filter: 'audioonly'});
-    dispatcher = connection.play(music, musicOptions);
+    if (videoId) {
+      const connection = await voiceChannel.join();
+      const music = ytdl(`https://www.youtube.com/watch?v=${videoId}`, {filter: 'audioonly'});
+      const dispatcher = connection.play(music, musicOptions);
+      dispatcher.on('finish', async () => {
+        await nextMusic(voiceChannel);
+      });
+    } else { // if not music found
+      isPlaying = false;
+      return;
+    }
   } catch (error) {
     console.error(error);
+  }
+};
+
+const nextMusic = async (voiceChannel) => {
+  SONG_LIST_NAMES.shift();
+  if (SONG_LIST_NAMES?.length) {
+    await searchMusicByNameOnYoutube(voiceChannel, SONG_LIST_NAMES[0]);
+  } else {
+    isPlaying = false;
+    await voiceChannel.leave();
   }
 };
